4058dfe649d4e6e1867ca50dc0e905bc0d347ff0
ssaifee <ssaifee@covermymeds.com>
2024-05-21 16:20:13 -0400

    pus

--- scripts/ready-to-test.rb
+++ scripts/ready-to-test.rb
@@ -36,14 +36,39 @@ def filter_yaml_files(contents)
   contents.select { |item| item['type'] == 'file' && item['name'].match?(/testing/) }
 end
 
+def find(yaml_files)
+  already_reserved_by_branch = yaml_files.find do |file|
+    yaml_string = File.read file
+    data = YAML.load yaml_string
+    data.dig("metadata","annotations","reservedBy") && data.dig("metadata","annotations","reservedBy") == "myapp/mybranch"
+  end
+
+  return already_reserved_by_branch if already_reserved_by_branch
+
+  available = yaml_files.find do |file|
+    yaml_string = File.read file
+    data = YAML.load yaml_string
+    data.dig("metadata","annotations","reservedBy").nil? || data.dig("metadata","annotations","reservedBy").empty?
+  end
+end
+
+def commit_to_control(argo_file)
+  yaml_string = File.read argo_file
+  data = YAML.load yaml_string
+  data["metadata"]["annotations"]["reservedBy"] = "myapp/mybranch"
+  File.write(argo_file, data.to_yaml)
+
+  puts(File.read argo_file)
+end
+
 # Main script
 begin
   contents = fetch_folder_contents
   yaml_files = filter_yaml_files(contents)
 
-  yaml_files.each do |file|
-    puts "- #{file['name']}"
-  end
+  cluster_file = find(yaml_files)
+  # if cluster file is nil, post a commit saying no env found
+
 rescue StandardError => e
   puts "An error occurred: #{e.message}"
 end
